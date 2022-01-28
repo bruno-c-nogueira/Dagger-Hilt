44e9b76258f7086ab90dd741f1f13ed2450a2f83
bruno-c-nogueira <Bruno-C.Nogueira@btgpactual.com>
2022-01-28 16:44:49 -0300

    learning activity scope

--- app/src/main/java/com/example/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilt/MainActivity.kt
@@ -2,30 +2,35 @@ package com.example.daggerhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var someClass: SomeClass
 
-    @Inject
-    lateinit var someClass2: SomeOtherClass
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         someClass.showMe()
-        someClass2.showMe()
-
     }
 }
+@AndroidEntryPoint
+class MyFragment: Fragment(){
 
+    @Inject
+    lateinit var someOtherClass : SomeOtherClass
+}
 
 class SomeClass @Inject constructor(){
     fun showMe() = "I was instantiated with success"
 }
 
+@FragmentScoped
 class SomeOtherClass @Inject constructor(private val someClass: SomeClass){
     fun showMe() = "I was instantiated with construct arg ${someClass.showMe()}"
 }
